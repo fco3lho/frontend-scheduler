70e4a7fa6f8ca5faaa77c0b855c3b114cc320f77
Felipe Campos <felipecampos50123@gmail.com>
2023-11-04 19:25:20 -0300

    Simulação do FIFO acontecendo pelo log com os dados obtidos do backend

--- src/pages/FIFO/FIFO.js
+++ src/pages/FIFO/FIFO.js
@@ -15,16 +15,48 @@ const FifoScheduler = () => {
       });
   }, []);
 
-  const handleClick = () => {
-    setProcesses((prevProcesses) => [
-      ...prevProcesses.slice(1), // Move o primeiro elemento para o final
-      prevProcesses[0], // Coloca o primeiro elemento no final
-    ]);
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  var time = 0;
+  var totalTime = 0;
+
+  const handleClick = async() => {
+    for (var i = 0; i < processes.length; i++) {
+      // console.log("EXECUÇÃO DE NÚMERO ", i + 1);
+      // console.log("\nPID: ", processes[i].processID);
+      // console.log("\nTempo de execução: ", processes[i].executionTime);
+      // console.log("\nTempo de idle: ", processes[i].idleTime);
+      // console.log("\nTempo total de Quantum: ", processes[i].totalQuantum);
+      // console.log("\nExecução do processo finalizada: ", processes[i].ended);
+      // console.log(
+      //   "\nTempo total de execução: ",
+      //   processes[i].fullExecutionTime
+      // );
+      // console.log("\n\n");
+
+      time = 0;
+
+      for (var j = 0; j < processes[i].totalQuantum; j++) {
+        await sleep(10);
+
+        if(j % 1 === 0){
+          time = time + 1;
+          totalTime = totalTime + 1;
+
+          console.log("Processo sendo executado: ", processes[i].processID);
+          console.log("Tempo de execução do processo ", processes[i].processID, ": ", time);
+          console.log("Tempo total de processamento: ", totalTime);
+          console.log("\n");
+        }
+      }
+    }
   };
 
   return (
     <div className="fifo-scheduler">
-      <button onClick={handleClick}>Escalonar</button>
+      <button onClick={handleClick}>Clique aqui</button>
       <div className="element-container">
         {processes.length > 0 &&
           processes.map((item, index) => (
