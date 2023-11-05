b6ba90b1984691079c1f054e6b824e04d2deba1e
Pedro Henrique Louback Campos <pcampos952@gmail.com>
2023-11-05 11:49:17 -0300

    Correção de links da página Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
                     <div className="first-in-first">First In First Out</div>
                 </div>
                 <img className="image-1-icon" alt="" src={FifoImage} />
-                <Link to="/lottery" className="simular">
+                <Link to="/fifo" className="simular">
                     <div className="frame-group">
                         <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M4.13477 0.385966C3.29648 -0.11169 2.24297 -0.128096 1.3877 0.336748C0.532422 0.801591 0 1.67659 0 2.62815V21.8782C0 22.8297 0.532422 23.7047 1.3877 24.1696C2.24297 24.6344 3.29648 24.6125 4.13477 24.1203L20.4473 14.4953C21.2572 14.0196 21.75 13.1719 21.75 12.2532C21.75 11.3344 21.2572 10.4922 20.4473 10.011L4.13477 0.385966Z" fill="white"/>
@@ -62,7 +62,7 @@ const Home = () => {
                     <div className="first-in-first">Fila de prioridade</div>
                 </div>
                 <img className="priority-1-icon" alt="" src={Priority} />
-                <Link to="/lottery" className="simular">
+                <Link to="/priorityQueues" className="simular">
                     <div className="frame-group">
                         <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M4.13477 0.385966C3.29648 -0.11169 2.24297 -0.128096 1.3877 0.336748C0.532422 0.801591 0 1.67659 0 2.62815V21.8782C0 22.8297 0.532422 23.7047 1.3877 24.1696C2.24297 24.6344 3.29648 24.6125 4.13477 24.1203L20.4473 14.4953C21.2572 14.0196 21.75 13.1719 21.75 12.2532C21.75 11.3344 21.2572 10.4922 20.4473 10.011L4.13477 0.385966Z" fill="white"/>
@@ -76,7 +76,7 @@ const Home = () => {
                     <div className="first-in-first">Fair Share</div>
                 </div>
                 <img className="fair-share-1-icon" alt="" src={FairShare} />
-                <Link to="/lottery" className="simular">
+                <Link to="/FairShare" className="simular">
                     <div className="frame-group">
                         <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M4.13477 0.385966C3.29648 -0.11169 2.24297 -0.128096 1.3877 0.336748C0.532422 0.801591 0 1.67659 0 2.62815V21.8782C0 22.8297 0.532422 23.7047 1.3877 24.1696C2.24297 24.6344 3.29648 24.6125 4.13477 24.1203L20.4473 14.4953C21.2572 14.0196 21.75 13.1719 21.75 12.2532C21.75 11.3344 21.2572 10.4922 20.4473 10.011L4.13477 0.385966Z" fill="white"/>
